781e441dd8e549522f53a24ff4f7dc8dbc0b65f5
ystephie <ystephie@berkeley.edu>
2012-04-17 20:58:38 -0700

    added stuff jay wanted

--- request_house_data.php
+++ request_house_data.php
@@ -8,12 +8,14 @@ if (!$range) {
 }
 $bedrooms = $_POST["bedrooms"] == "true"? true: false; 
 $houses = $_POST["houses"] == "true"? true: false;
+$r_units = $_POST["r_units"] == "true"? true: false;
 $address = $_POST["address"] == "true"? true: false;
 $house_size = $_POST["house_size"] == "true"? true: false;
 $lot_size = $_POST["lot_size"] == "true"? true: false;
 $br = $_POST["br"] == "true"? true: false;
 $bathrooms = $_POST["bathrooms"] == "true"? true: false;
 $year_built = $_POST["year_built"] == "true"? true: false;
+$s_units = $_POST["s_units"] == "true"? true: false;
 
 //connect to sql server
 $con = mysql_connect("lager.cs.berkeley.edu","jeffrey.nieh","housedata");
@@ -26,7 +28,7 @@ mysql_select_db("housedata", $con);
 //logic
 
 if ($house_size || $address || $lot_size || $br || $bathrooms || $year_built) {
-	$result = mysql_query("SELECT * FROM house_data as H, location_data as L WHERE H.zpid=L.zpid ORDER BY ((ACOS(SIN($latitude * PI() / 180) * SIN(latitude * PI() / 180) + COS($latitude * PI() / 180) * COS(latitude * PI() / 180) * COS(($longitude - longitude ) * PI() / 180)) * 180 / PI()) * 60 * 1.1515) LIMIT 1");
+	$result = mysql_query("SELECT * FROM house_data as H, location_data as L, house_data_more_facts AS M WHERE H.zpid=L.zpid and H.zpid=M.zpid ORDER BY ((ACOS(SIN($latitude * PI() / 180) * SIN(latitude * PI() / 180) + COS($latitude * PI() / 180) * COS(latitude * PI() / 180) * COS(($longitude - longitude ) * PI() / 180)) * 180 / PI()) * 60 * 1.1515) LIMIT 1");
 	
 	$row = mysql_fetch_array($result);
 	if ($address) {
@@ -47,18 +49,26 @@ if ($house_size || $address || $lot_size || $br || $bathrooms || $year_built) {
 	if ($year_built) {
 		$html .= "Year Built: " . $row["year_built"] . "<br>";
 	}
+	if ($s_units) {
+		$html .= "Units: " . $row["c_unit_count"] . "<br>";
+	}
 }
 
 
 if ($bedrooms || $houses) {
-	$result = mysql_query("SELECT SUM(bedrooms) as bedroom_cnt, count(H.zpid) as house_cnt FROM house_data AS H, location_data AS L WHERE H.zpid=L.zpid AND ((ACOS(SIN($latitude * PI() / 180) * SIN(latitude * PI() / 180) + COS($latitude * PI() / 180) * COS(latitude * PI() / 180) * COS(($longitude - longitude ) * PI() / 180)) * 180 / PI()) * 60 * 1.1515)<=$range");
+	$result = mysql_query("SELECT SUM(bedrooms) as bedroom_cnt, count(H.zpid) as house_cnt, SUM(c_unit_count) as c_units FROM house_data AS H, location_data AS L , house_data_more_facts AS M WHERE H.zpid=L.zpid AND H.zpid=M.zpid AND ((ACOS(SIN($latitude * PI() / 180) * SIN(latitude * PI() / 180) + COS($latitude * PI() / 180) * COS(latitude * PI() / 180) * COS(($longitude - longitude ) * PI() / 180)) * 180 / PI()) * 60 * 1.1515)<=$range");
+	
+	$html .= "Range Info (within a " . $range . " mi radius): <br>";
 	
 	$row = mysql_fetch_array($result);
 	if ($bedrooms) {
-		$html .= "Nearby Bedrooms: " . $row["bedroom_cnt"] . "<br>";
+		$html .= "Bedrooms: " . $row["bedroom_cnt"] . "<br>";
 	}
 	if ($houses) {
-		$html .= "Number of Buildings: " . $row["house_cnt"] . "<br>";
+		$html .= "Buildings: " . $row["house_cnt"] . "<br>";
+	}
+	if ($r_units) {
+		$html .= "Units: " . $row["c_units"] . "<br>";
 	}
 }
 
